2329357113dbe62a0f8367a0b4493153816a09ea
Bare7a <bare7a@gmail.com>
2023-06-17 21:04:25 +0300

    Updated preinstall script

--- preinstall.sh
+++ preinstall.sh
@@ -1,8 +1,14 @@
 sudo rm /etc/resolv.conf
-sudo bash -c 'echo "nameserver 8.8.8.8" > /etc/resolv.conf'
-sudo bash -c 'echo "nameserver 8.8.4.4" >> /etc/resolv.conf'
 sudo bash -c 'echo "[network]" > /etc/wsl.conf'
-sudo bash -c 'echo "generateResolvConf = false" >> /etc/wsl.conf'
+sudo bash -c 'echo "nameserver 8.8.8.8" >> /etc/resolv.conf'
+sudo bash -c 'echo "nameserver 8.8.4.4" >> /etc/resolv.conf'
 sudo chattr +i /etc/resolv.conf
+
+sudo rm /etc/wsl.conf
+sudo bash -c 'echo "[boot]" > /etc/wsl.conf'
+sudo bash -c 'echo "systemd=true" >> /etc/wsl.conf'
+sudo bash -c 'echo "[network]" >> /etc/wsl.conf'
+sudo bash -c 'echo "generateResolvConf = false" >> /etc/wsl.conf'
+
 echo 'Please do "exit" then'
 echo 'wsl --shutdown'
